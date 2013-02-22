be80ce5e33ef6d0d2485a9ef3d0eb08d0c058d30
Apprentice <apprentice@dbc22.local>
2013-02-22 01:30:43 -0800

    Working on bug in posts.rb for my edit page

--- blog_skeleton/app/controllers/posts.rb
+++ blog_skeleton/app/controllers/posts.rb
@@ -5,33 +5,51 @@ get '/post/new' do
 end
 
 post '/post' do
-  @newpost = Post.new(title: params[:title],
+  puts "[LOG POST] #{params}" 
+  #BUGBUG not reading in the post id from the edit post page pull that in and make it appear here!
+  if params[:post_id] == " "
+    @newpost = Post.new(title: params[:title],
                       content: params[:content])
-  @newpost.save
+    @newpost.save
   # puts "[LOG TAGS] #{params[:tags]}"
-  tags = params[:tags].split(/[\s,]+/)
+    tags = params[:tags].split(/[\s,]+/)
   # puts "[LOG TAGS SPLIT] #{tags}"
-  tags.each do |tag|
-    @newpost.tags << Tag.create(description: tag)
+    tags.each do |tag|
+      @newpost.tags << Tag.create(description: tag)
+    end
+    # going to have to put a validation for unique tag and if error how to handle it
+    redirect "/post/#{@newpost.id}"
+  else
+    @post = Post.find_by_id(params[:post_id])
+    @post.update_attributes(title: params[:title],
+                            content: params[:content])
+    tags.each do |tag|
+      @post.tags << Tag.create(description: tag)
+    end
+    redirect "/post/#{@post.id}"
   end  
   # puts "[LOG] #{@newpost.title}"
   # puts "[LOG] #{@newtag.description}"
-  redirect "/post/#{@newpost.id}"
-
 end
 
 get '/post/:post_id' do
-  puts "[LOG] #{params[:post_id]}"
+  puts "[LOG WHAT] #{params[:post_id]}"
   @post = Post.find_by_id(params[:post_id])
   erb :post
 end  
 
-post '/post/edit' do
-  # update the attributes that were rewritten
-  # redirect back to it's own page
+get '/post/edit/:post_id' do
+  @post = Post.find_by_id(params[:post_id])
+  erb :edit
 end
 
-  
+post '/post/delete' do
+
+
+end
+
+
+
 
 
 
